a5fad7b9897e60695a650086725c49098016edc9
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-06-02 02:57:06 +0500

    added 29 commit

--- Class-02 Chatbot with Agents SDK/chain_hello/hello.py
+++ Class-02 Chatbot with Agents SDK/chain_hello/hello.py
@@ -1,12 +1,18 @@
 import chainlit as cl
 import os
 
-from agents import Agent, RunConfig, OpenAIChatCompletionsModel
+from agents import Agent, RunConfig, AsyncOpenAI,OpenAIChatCompletionsModel
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
 gemini_api_key= os.getenv("GEMINI_API_KEY")
+
+provider = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
 
 @cl.on_message
 async def main(message: cl.Message):
