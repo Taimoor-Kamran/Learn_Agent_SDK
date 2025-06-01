108e06018bfc1ac8209d71a497ebcb8e1a0c0692
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-06-02 02:49:14 +0500

    added 27 commit

--- Class-02 Chatbot with Agents SDK/chain_hello/hello.py
+++ Class-02 Chatbot with Agents SDK/chain_hello/hello.py
@@ -3,6 +3,7 @@ import chainlit as cl
 from agents import Agent, RunConfig, OpenAIChatCompletionsModel
 from dotenv import load_dotenv, find_dotenv
 
+load_dotenv(find_dotenv())
 
 @cl.on_message
 async def main(message: cl.Message):
